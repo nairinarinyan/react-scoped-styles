7d26036ae6d0a685012dd5ffdb0a0973a1095ef1
Nairi Narinyan <nairi.narinyan@gmail.com>
2019-02-11 17:32:07 +0400

    add inline classstring transform for conditional classes etc.

--- src/script-loader.ts
+++ src/script-loader.ts
@@ -3,23 +3,27 @@ import { LoaderContext } from './options';
 
 function scriptLoader(this: LoaderContext, source: string): string {
     const { globalsPrefix = 'app' }  = this.query;
-    const classNameRegex = /(className.*?["|'])(.*?)("|')/g;
 
-    if (!source.match(classNameRegex)) {
+    const classExprRegex = /className.*?['|"].*?['|"].*?[:|}]/g
+    const classStringRegex = new RegExp(`['|"](.*?)['|"]`, 'g')
+
+    if (!source.match(classExprRegex)) {
         return source;
     }
 
     const [dirName, dirHash] = createDirHash(this.context);
-    return source.replace(classNameRegex, (_match, p1, classNames, p3) => {
-        const uniqueClassNames = classNames.split(' ')
-            .map((className: string) => {
-                const containsPrefix = className.startsWith(`${globalsPrefix}-`);
-                const uniqueClassName = `${dirName}-${dirHash}-${className}`;
-                return containsPrefix ? className : uniqueClassName;
-            })
-            .join(' ');
+    return source.replace(classExprRegex, (classExpr) => {
+        return classExpr.replace(classStringRegex, (_match, classNames) => {
+            const uniqueClassNames = classNames.split(' ')
+                .map((className: string) => {
+                    const containsPrefix = className.startsWith(`${globalsPrefix}-`);
+                    const uniqueClassName = `${dirName}-${dirHash}-${className}`;
+                    return containsPrefix ? className : uniqueClassName;
+                })
+                .join(' ');
 
-        return p1 + uniqueClassNames + p3;
+            return "'" + uniqueClassNames + "'";
+        });
     });
 }
 
