b4a2ee14c82fee84445832090d08a33184adb138
Nairi Narinyan <nairi.narinyan@gmail.com>
2021-05-09 16:24:41 +0400

    hash by relative path

--- src/lib/dirhash.ts
+++ src/lib/dirhash.ts
@@ -1,8 +1,9 @@
-import { parse } from 'path';
+import { parse, relative } from 'path';
 import { createHash } from 'crypto';
 
-export function createDirHash(filePath: string): [string, string] {
-    const { dir: dirPath, name: dirName} = parse(filePath);
+export function createDirHash(contextPath: string): [string, string] {
+    const dirPath = relative(process.cwd(), contextPath);
+    const { name: dirName} = parse(dirPath);
     const dirHash = createHash('md5').update(dirPath).digest('hex').slice(0, 10);
     return [dirName, dirHash];
 }
