6c404e195b22c0b27a3d3e67f29c5219a32f0157
Nairi Narinyan <nairi.narinyan@gmail.com>
2021-10-20 00:52:31 +0400

    simplify style class matching

--- src/style-loader.ts
+++ src/style-loader.ts
@@ -2,19 +2,25 @@ import { loader } from 'webpack';
 import { getInclude } from './lib/include-hash';
 import { createDirHash } from './lib/dirhash';
 
-const classLineRegex = /(?<!:\s.*?)(\..*?(?<!;)$)/gm;
-const classRegex = /(?<=\.)\S+?(?=$|\s|{)/g;
+const classRegex = /(?<=\.)[_a-zA-Z]+[_a-zA-Z0-9-]*/g;
+const ruleBodyRegex = /\{.*?\}/g;
 
 export default function styleLoader(this: loader.LoaderContext, source: string): string {
     const includeHash = getInclude(this);
 
-    if (!source.match(classLineRegex)) {
-        return source;
-    }
-
+    const strippedSrc = source.replace(ruleBodyRegex, '');
     const dirHash = createDirHash(this.context);
 
-    return source.replace(classLineRegex, matchingLine =>
-        matchingLine.replace(classRegex, className => includeHash(className, dirHash))
-    );
+    const classes = strippedSrc
+        .match(classRegex)?.reduce((acc, curr) => {
+            return acc.includes(curr) ? acc : acc.concat(curr);
+        }, [] as string[]);
+    
+    let ret = source;
+
+    classes?.forEach(className => {
+        ret = ret.replace(new RegExp(className, 'g'), c => includeHash(c, dirHash));
+    });
+
+    return ret;
 };
